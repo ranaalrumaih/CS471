646c695d832e8831b453cb1337a39fdece1692dd
ranaalrumaih <ranaalrumaih44@gmail.com>
2026-09-22 08:41:41 +0300

    Complete Django URL and template lab3

--- bookwebsite/urls.py
+++ bookwebsite/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import apps.bookmodule.views
+from django.urls import include, path
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', include("apps.bookmodule.urls")),
+    path('users/', include("apps.usermodule.urls"))
 ]
